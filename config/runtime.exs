0659da17aa9ce558b79cc6a9b3803ab0e7dab603
josepharodi20 <josepharodi20@gmail.com>
2024-02-28 20:54:02 +0300

    Reverted runtime.exs file

--- config/runtime.exs
+++ config/runtime.exs
@@ -68,14 +68,14 @@ if config_env() == :prod do
   # To get SSL working, you will need to add the `https` key
   # to your endpoint configuration:
   #
-      config :auth_system, AuthSystemWeb.Endpoint,
-        https: [
-          ...,
-          port: 443,
-          cipher_suite: :strong,
-          keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
-          certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
-        ]
+      # config :auth_system, AuthSystemWeb.Endpoint,
+      #   https: [
+      #     ...,
+      #     port: 443,
+      #     cipher_suite: :strong,
+      #     keyfile: System.get_env("SOME_APP_SSL_KEY_PATH"),
+      #     certfile: System.get_env("SOME_APP_SSL_CERT_PATH")
+      #   ]
   #
   # The `cipher_suite` is set to `:strong` to support only the
   # latest and more secure SSL ciphers. This means old browsers
