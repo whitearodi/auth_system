ef69c1b0d7dae910bc5049cd34107d0c7dfe96b7
josepharodi20 <josepharodi20@gmail.com>
2024-02-29 21:26:59 +0300

    Added script to prod.exs

--- config/prod.exs
+++ config/prod.exs
@@ -6,7 +6,8 @@ import Config
 # which you should run after static files are built and
 # before starting your production server.
 config :auth_system, AuthSystemWeb.Endpoint,
-url: [host: "https://auth-system.gigalixirapp.com"],
+  url: [host: "https://auth-system.gigalixirapp.com"],
+ check_origin: ["https://auth-system.gigalixirapp.com"],
   force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/cache_manifest.json"
 
